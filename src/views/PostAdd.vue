412cce008ab7547f767c61833445896a65ee4930
Will-Z-fighting <53569286+Will-Z-fighting@users.noreply.github.com>
2019-10-09 16:20:12 +0800

    添加富文本编辑器和视频文章发布

--- src/views/PostAdd.vue
+++ src/views/PostAdd.vue
@@ -5,20 +5,41 @@
                 <el-input v-model="form.title"></el-input>
             </el-form-item>
 
-        <el-form-item label="内容">
-            <el-input v-model="form.content" rows="5" type="textarea"></el-input>
-        </el-form-item>
+            <el-form-item label="类型">
+                <el-radio-group v-model="form.type">
+                    <el-radio :label="1">文章</el-radio>
+                    <el-radio :label="2">视频</el-radio>
+                </el-radio-group>
+            </el-form-item>
+
+            <el-form-item label="内容" v-if="form.type === 1">
+                <!-- <el-input v-model="form.content" rows="5" type="textarea"></el-input> -->
+                <VueEditor :config="config"  ref="vueEditor"/>
+            </el-form-item>
+
+            <el-form-item label="视频" v-if="form.type === 2">
+                <el-upload
+                    :action="`$($axios.defaults.baseURL)/upload`"
+                    name="file"
+                    :headers="{
+                        Authorization: token
+                    }"
+                    :on-success="handleVideoSuccess"
+                    >
+                    <el-button size="small" type="primary">点击上传</el-button>
+                </el-upload>
+            </el-form-item>
 
             <el-form-item label="栏目">
                 <!-- 栏目的数据来自于后台 -->
                 <el-checkbox-group v-model="form.categories">
-                <!-- allCate所有的栏目 -->
-                <el-checkbox 
-                v-for="(item, index) in allCate"
-                :key="index"
-                v-if="item.id !== 999"
-                :label="item.id" 
-                name="type">{{item.name}}</el-checkbox>
+                    <!-- allCate所有的栏目 -->
+                    <el-checkbox 
+                    v-for="(item, index) in allCate"
+                    :key="index"
+                    v-if="item.id !== 999"
+                    :label="item.id" 
+                    name="type">{{item.name}}</el-checkbox>
                 </el-checkbox-group>
             </el-form-item>
 
@@ -28,24 +49,19 @@
                 on-success： 图片上传成功的函数
                 on-remove: 移除图片函数 -->
                 <el-upload
-                action="http://localhost:3000/upload"
-                name="file"
-                :headers="{
-                    Authorization: token
-                }"
-                list-type="picture-card"
-                :on-success="handleSuccess"
-                :on-remove="handleRemove">
-                <i class="el-icon-plus"></i>
+                    :action="`${$axios.defaults.baseURL}/upload`"
+                    name="file"
+                    :headers="{
+                        Authorization: token
+                    }"
+                    list-type="picture-card"
+                    :on-success="handleSuccess"
+                    :on-remove="handleRemove">
+                    <i class="el-icon-plus"></i>
                 </el-upload>
             </el-form-item>
 
-            <el-form-item label="类型">
-                <el-radio-group v-model="form.type">
-                <el-radio :label="1">文章</el-radio>
-                <el-radio :label="2">视频</el-radio>
-                </el-radio-group>
-            </el-form-item>
+
 
             <el-form-item>
                 <el-button type="primary" @click="onSubmit">立即创建</el-button>
@@ -55,6 +71,9 @@
 </template>
 
 <script>
+// 导入富文本编辑器
+import VueEditor from "vue-word-editor";
+import "quill/dist/quill.snow.css"
 
 export default {
     name: 'app',    
@@ -75,7 +94,40 @@ export default {
             // token
             token: JSON.parse(localStorage.getItem(`user`) || `{}`).token,
 
+            // 编辑器的配置
+            config: {
+                // 上传图片的配
+                uploadImage: {
+                    url: this.$axios.defaults.baseURL + "/upload",
+                    name: "file",
+                    headers: {
+                        Authorization: JSON.parse(localStorage.getItem("user") || `{}`).token
+                    },
+                    // res是结果，insert方法会把内容注入到编辑器中，res.data.url是资源地址
+                    uploadSuccess: (res, insert) => {
+                        insert(this.$axios.defaults.baseURL + res.data.data.url)
+                    }
+                },
+
+                // 上传视频的配置
+                uploadVideo: {
+                    url: this.$axios.defaults.baseURL + "/upload",
+                    name: "file",
+                    headers: {
+                        Authorization: JSON.parse(localStorage.getItem("user") || `{}`).token
+                    },
+                    // res是结果，insert方法会把内容注入到编辑器中，res.data.url是资源地址
+                    uploadSuccess: (res, insert) => {
+                        insert(this.$axios.defaults.baseURL + res.data.data.url)
+                    }
+                },
+            },
+
         }
+    },
+
+    components: {
+        VueEditor
     },
     
     methods: {
@@ -89,6 +141,11 @@ export default {
                 id: v
                 })
             });
+
+            // 使用refs获取编辑器中的内容
+            if(this.form.type === 1){
+                this.form.content = this.$refs.vueEditor.editor.root.innerHTML;
+            }
 
             this.$axios({
                 url: "/post",
@@ -123,7 +180,14 @@ export default {
             this.form.cover.push({
                 id: res.data.id
             })
-        }
+        },
+
+        // 上传视频
+        handleVideoSuccess(res){
+            // 把视频链接保存到content
+            this.form.content = this.$axios.defaults.baseURL + res.data.url;
+
+        },
     },
 
     mounted(){
